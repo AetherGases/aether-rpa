375eed6f6e53feafece68860a6a094fa3749d94f
eduardocostaam <eduardocosta942z@gmail.com>
2026-09-12 22:23:05 -0300

    pg_perm

--- permission/definition.py
+++ permission/definition.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 @dataclass
-class RegisterA:
+class Register:
     id: int
     name: str
     description: str | None
@@ -13,20 +13,5 @@ class RegisterA:
 
 
 @dataclass
-class TableA:
-    registers: list[RegisterA]
-
-
-@dataclass
-class RegisterB:
-    id: int
-    name: str
-    description: str | None
-    url: str
-    created_at: datetime
-    updated_at: datetime | None
-
-
-@dataclass
-class TableB:
-    registers: list[RegisterB]
+class Table:
+    registers: list[Register]
